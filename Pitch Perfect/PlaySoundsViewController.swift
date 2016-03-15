7d3da8e3efa0f6bed5e4f57dfc5630812e973049
rsaccoll <rsaccoll@gmail.com>
2016-03-15 17:05:49 -0300

    More crap (a few error on the PlaySoundsViewController, but less then the last commit) ¯\_(ツ)_/¯

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -11,26 +11,28 @@ import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
 
+    var audioPlayer:AVAudioPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-       if var filePath =  NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3")
-        
-       {
-        var filePathUrl = NSURL.fileURLWithPath(filePath)
-        var audioPlayer = AVAudioPlayer(contentsOfURL: <#T##NSURL#>)
-       } else {
-        println("the filePath is empty")
+        if var filePath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3") {
+            var filePathUrl = NSURL.fileURLWithPath(filePath)
+            audioPlayer = AVAudioPlayer(contentsOfURL: filePathUrl)
+            audioPlayer.enableRate
+            
+            
+        } else {
+            print("the filepath is empty")
         }
-    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     @IBAction func playSlowAudio(sender: UIButton) {
-        audioPlayer.play
+        
     }
     /*
     // MARK: - Navigation
