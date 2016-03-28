4e34fe8fab4a91f8ca942ae06b1b7b69e0cda3c7
rsaccoll <rsaccoll@gmail.com>
2016-03-28 16:34:55 -0300

    Using the new version of the course. Need some "workaround"...eehehhe

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -13,27 +13,22 @@ class PlaySoundsViewController: UIViewController {
 
     var audioPlayer:AVAudioPlayer!
     
+    func recordAudio() {
+        var audioRecorder: AVAudioRecorder!
+        
+        let dirPath = NSSearchPathDirectoriesInDomains(NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true) [0] as String,
+        let recordingName ""
+        let pathArray = [dirPath, recordingName]
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        if var filePath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3") {
-            var filePathUrl = NSURL.fileURLWithPath(filePath)
-            audioPlayer = AVAudioPlayer(contentsOfURL: filePathUrl)
-            audioPlayer.enableRate
-            
-            
-        } else {
-            print("the filepath is empty")
-        }
+
+    }
 
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    @IBAction func playSlowAudio(sender: UIButton) {
-        
-    }
     /*
     // MARK: - Navigation
 
